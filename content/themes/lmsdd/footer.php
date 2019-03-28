9cf79f30bad0614974c77f23dbea3b88ffe1719c
LunarJim <jim.deghaye@gmail.com>
2019-03-28 10:51:32 +0100

    modif css html et footer + positions

--- content/themes/lmsdd/footer.php
+++ content/themes/lmsdd/footer.php
@@ -5,7 +5,7 @@
 
     <div class="container-fluid text-uppercase footer-top">
 
-      <ul class="list-unstyled list-inline text-center">
+      <ul class="list-unstyled list-inline text-center mb-0">
         <li class="list-inline-item">
           <a class="btn-floating btn-fb mx-1">
             <i class="fab fa-facebook-f"> </i>
--- content/themes/lmsdd/front-page.php
+++ content/themes/lmsdd/front-page.php
@@ -1,138 +1,49 @@
-<?php get_header(); ?>
+<?php get_header();?>
 
-  <div class="main container posts">
+<div class="main container posts">
+  <div class="row mb-4 pb-3">
 
-    <div class="row mb-4 pb-3">
 
-      <div class="col-lg-4 col-sm-6 posts__sticker">
-        <div class="posts__content bg-warning pl-1 pr-1 pt-1 pb-1 rounded shadow  bg-warning rounded">
-            Lorem ipsum dolor sit yo amet, consectetur adipisicing elit. Dolores eos molestias, rem porro, illum sequi mollitia unde iste voluptatibus quibusdam, laudantium error recusandae nemo. Voluptas, voluptatibus. Quod iure facere corrupti eligendi solut
-            <hr>
-            <div class=" row ml-3 post-rate">
-              <a href=""> <button type="button" class="btn btn-secondary mb-1 mr-1 pl-1 pr-1">Classique</button></a> 
-              <a href=""><button type="button" class="btn btn-secondary mb-1 pl-1 pr-1">Humour</button></a>
-            </div>
-            <div class="container">
-              <button type="button" class="btn btn-primary">
-              <i class="fas fa-heart"></i> <span class="badge badge-success">4</span>
-              </button>
-              <button type="button" class="btn btn-primary">
+
+  <?php
+
+  // query des cpt quotes
+
+  $args = [
+    'post_type'  => 'quote',
+    'posts_per_page' => 6,
+  ];
+
+  $quotes = new WP_Query($args);
+
+  if ($quotes->have_posts()) : while ($quotes->have_posts()): $quotes->the_post();
+  ?>
+
+
+    <div class="col-lg-4 col-sm-6 posts__sticker">
+      <div class="posts__content bg-warning pl-1 pr-1 pt-1 pb-1 rounded shadow  bg-warning rounded">
+        <?php the_content(); ?>
+          <hr>
+          <div class=" row ml-3 post-rate">
+            <a href=""> <button type="button" class="btn btn-secondary mb-1 mr-1 pl-1 pr-1">Classique</button></a>
+            <a href=""><button type="button" class="btn btn-secondary mb-1 pl-1 pr-1">Humour</button></a>
+          </div>
+          <div class="container">
+            <button type="button" class="btn btn-primary">
+            <i class="fas fa-heart"></i> <span class="badge badge-success">4</span>
+            </button>
+            <button type="button" class="btn btn-primary">
               <i class="fas fa-heart-broken"></i> <span class="badge badge-danger">4</span>
-              </button>
-              <button type="button" class="btn btn-primary">
+            </button>
+            <button type="button" class="btn btn-primary">
               <i class="fas fa-temperature-high"></i> <span class="badge badge-danger">4</span>
-              </button>
+            </button>
               <a class="ml-3" href=""><i class="fas fa-share-alt share"></i></a>
             </div>
         </div>
       </div>
-
-      <div class="col-lg-4 col-sm-6 posts__sticker">
-          <div class="posts__content bg-warning pl-1 pr-1 pt-1 pb-1 rounded shadow  bg-warning rounded">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores eos molestias, rem porro, illum sequi mollitia unde iste voluptatibus quibusdam, laudantium error recusandae nemo. Voluptas, voluptatibus. Quod iure facere corrupti eligendi solut
-              <hr>
-              <div class="post-rate justify-content-center">
-                <a href=""> <button type="button" class="btn btn-secondary mb-1">Classique</button></a> 
-                <a href=""><button type="button" class="btn btn-secondary mb-1">Humour</button></a>
-                <br>
-                <button type="button" class="btn btn-primary">
-                <i class="fas fa-heart"></i> <span class="badge badge-success">4</span>
-                </button>
-                <button type="button" class="btn btn-primary">
-                <i class="fas fa-heart-broken"></i> <span class="badge badge-danger">4</span>
-                </button>
-                <button type="button" class="btn btn-primary">
-                <i class="fas fa-temperature-high"></i> <span class="badge badge-danger">4</span>
-                </button>
-              </div>
-          </div>
-        </div>
-
-      <div class="col-lg-4 col-sm-6 posts__sticker">
-          <div class="posts__content bg-warning pl-1 pr-1 pt-1 pb-1 rounded shadow  bg-warning rounded">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores eos molestias, rem porro, illum sequi mollitia unde iste voluptatibus quibusdam, laudantium error recusandae nemo. Voluptas, voluptatibus. Quod iure facere corrupti eligendi solut
-              <hr>
-              <div class="post-rate justify-content-center">
-                <a href=""> <button type="button" class="btn btn-secondary mb-1">Classique</button></a> 
-                <a href=""><button type="button" class="btn btn-secondary mb-1">Humour</button></a>
-                <br>
-                <button type="button" class="btn btn-primary">
-                <i class="fas fa-heart"></i> <span class="badge badge-success">4</span>
-                </button>
-                <button type="button" class="btn btn-primary">
-                <i class="fas fa-heart-broken"></i> <span class="badge badge-danger">4</span>
-                </button>
-                <button type="button" class="btn btn-primary">
-                <i class="fas fa-temperature-high"></i> <span class="badge badge-danger">4</span>
-                </button>
-              </div>
-          </div>
-        </div>
-
-      <div class="col-lg-4 col-sm-6 posts__sticker">
-          <div class="posts__content bg-warning pl-1 pr-1 pt-1 pb-1 rounded shadow  bg-warning rounded">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores eos molestias, rem porro, illum sequi mollitia unde iste voluptatibus quibusdam, laudantium error recusandae nemo. Voluptas, voluptatibus. Quod iure facere corrupti eligendi solut
-              <hr>
-              <div class="post-rate justify-content-center">
-                <a href=""> <button type="button" class="btn btn-secondary mb-1">Classique</button></a> 
-                <a href=""><button type="button" class="btn btn-secondary mb-1">Humour</button></a>
-                <br>
-                <button type="button" class="btn btn-primary">
-                <i class="fas fa-heart"></i> <span class="badge badge-success">4</span>
-                </button>
-                <button type="button" class="btn btn-primary">
-                <i class="fas fa-heart-broken"></i> <span class="badge badge-danger">4</span>
-                </button>
-                <button type="button" class="btn btn-primary">
-                <i class="fas fa-temperature-high"></i> <span class="badge badge-danger">4</span>
-                </button>
-              </div>
-          </div>
-        </div>
-
-      
-      <div class="col-lg-4 col-sm-6 posts__sticker">
-          <div class="posts__content bg-warning pl-1 pr-1 pt-1 pb-1 rounded shadow  bg-warning rounded">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores eos molestias, rem porro, illum sequi mollitia unde iste voluptatibus quibusdam, laudantium error recusandae nemo. Voluptas, voluptatibus. Quod iure facere corrupti eligendi solut
-              <hr>
-              <div class="post-rate justify-content-center">
-                <a href=""> <button type="button" class="btn btn-secondary mb-1">Classique</button></a> 
-                <a href=""><button type="button" class="btn btn-secondary mb-1">Humour</button></a>
-                <br>
-                <button type="button" class="btn btn-primary">
-                <i class="fas fa-heart"></i> <span class="badge badge-success">4</span>
-                </button>
-                <button type="button" class="btn btn-primary">
-                <i class="fas fa-heart-broken"></i> <span class="badge badge-danger">4</span>
-                </button>
-                <button type="button" class="btn btn-primary">
-                <i class="fas fa-temperature-high"></i> <span class="badge badge-danger">4</span>
-                </button>
-              </div>
-          </div>
-        </div>
-
-      <div class="col-lg-4 col-sm-6 posts__sticker">
-          <div class="posts__content bg-warning pl-1 pr-1 pt-1 pb-1 rounded shadow  bg-warning rounded">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores eos molestias, rem porro, illum sequi mollitia unde iste voluptatibus quibusdam, laudantium error recusandae nemo. Voluptas, voluptatibus. Quod iure facere corrupti eligendi solut
-              <hr>
-              <div class="post-rate justify-content-center">
-                <a href=""> <button type="button" class="btn btn-secondary mb-1">Classique</button></a> 
-                <a href=""><button type="button" class="btn btn-secondary mb-1">Humour</button></a>
-                <br>
-                <button type="button" class="btn btn-primary">
-                <i class="fas fa-heart"></i> <span class="badge badge-success">4</span>
-                </button>
-                <button type="button" class="btn btn-primary">
-                <i class="fas fa-heart-broken"></i> <span class="badge badge-danger">4</span>
-                </button>
-                <button type="button" class="btn btn-primary">
-                <i class="fas fa-temperature-high"></i> <span class="badge badge-danger">4</span>
-                </button>
-              </div>
-          </div>
-      </div>
-    </div>
+      <?php endwhile; wp_reset_postdata(); endif; ?>
   </div>
-  
-<?php get_footer(); ?>
+</div>
+
+<?php get_footer();?>
