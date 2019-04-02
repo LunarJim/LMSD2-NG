74aad14221a958d640a8a16c735f25dfec2b9080
LunarJim <jim.deghaye@gmail.com>
2019-04-02 15:02:52 +0200

    préparation du form de quote submit + page 404

--- content/themes/lmsdd/template-submit.php
+++ content/themes/lmsdd/template-submit.php
@@ -13,38 +13,47 @@ Template Post Type: page
       <div class="col-md-6 submit-post mx-auto">
         
             <p>
-                <h4 class="text-center">
+                <h4 class="text-center text-white">
                   A toi de jouer : 
                 </h4> 
+
+                <!-- mode d'emploi avant publication -->
+
                 <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
                   mode d'emploi ici
                 </button>
             </p>
               <div class="collapse" id="collapseExample">
                 <div class="card card-body">
-                  Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+                  <ul class="list-group">
+                    <li class="list-group-item">Les citations sont toutes modérées : tâchons de rester poli ! Du fait de la modération, les citations ne sont pas publiées dès soumission mais sous 48 heures</li>
+                    <li class="list-group-item">La publication est possible sans être connecté en tant que membre. Cependant, nous vous recommandons de <a href="<?= get_page_link(11); ?>">créer un compte</a>  afin de pouvoir retrouver vos propres citations et de disposer de tous nos services perso miam miam !</li>
+                    <li class="list-group-item">Votre citation ne doit pas dépasser 120 caractères et si possible ne pas comporter de fautes (go Bescherelle)</li>
+                  </ul>
                 </div>
               </div>
+
+          <!-- formulaire de soumission de citation -->
         
           <form>
               <div class="form-group">
-                <label for="email">email</label>
-                <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email">
+                <label for="email" class="text-white">Email</label>
+                <input type="email" class="form-control" name="email" id="email" aria-describedby="emailHelp" placeholder="Email" required>
                 <small id="emailHelp" class="form-text text-muted"></small>
               </div>
               <div class="form-group">
-                <label for="exampleInputPassword1">Pseudo</label>
-                <input type="text" class="form-control" id="" placeholder="pseudo">
+                <label for="Pseudo" class="text-white">Pseudo</label>
+                <input type="text" class="form-control" id="" placeholder="Pseudo" name="author" required>
               </div>
               <div class="form-group">
-                  <label for="exampleFormControlTextarea1">Ta citation</label>
-                  <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                  <label for="exampleFormControlTextarea1" class="text-white">Ta citation (doit commencer par "ceux qui" ou "celles qui")</label>
+                  <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" name="cptContent" required></textarea>
                 </div>
               <div class="form-group form-check">
-                <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                <label class="form-check-label" for="exampleCheck1">M'avertir lors de la publication !</label>
+                <input type="checkbox" class="form-check-input" id="Check_publish">
+                <label class="form-check-label text-white font-italic" for="exampleCheck1" class="text-white">M'avertir lors de la publication !</label>
               </div>
-              <button type="submit" class="btn btn-primary">Go !</button>
+              <button type="submit" class="btn btn-primary mb-2">Go !</button>
             </form>
       </div>
     </div>
