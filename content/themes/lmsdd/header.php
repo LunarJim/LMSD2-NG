e62ec54185b4b300658ad9c376f7f344f611dad1
LunarJim <jim.deghaye@gmail.com>
2019-04-09 16:12:27 +0200

    harmonisation header (tout boutons même catégories) + harmonisation affichage du toggle

--- content/themes/lmsdd/header.php
+++ content/themes/lmsdd/header.php
@@ -6,8 +6,6 @@
   <link href="https://fonts.googleapis.com/css?family=Acme" rel="stylesheet">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-
-
 <?php wp_head(); ?>
 </head>
 
@@ -20,37 +18,67 @@
             </h1>
             <p class="font-italic logo__slogan">Le monde se divise en 2</p>
           </a>
-          <ul class="navbar-nav ml-5">
-              <li class="nav-item active dropdown">
-                <a class="nav-link dropdown-toggle text-uppercase" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  Catégories
-                </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          
 
-                <?php $categories = $terms = get_Terms(array(
-                  'taxonomy' => 'categorie',
-                  'hide_empty' => false,
-                ));
+          <!-- toggle button -->
 
-                if(is_array($categories)):
-
-                foreach ($categories as $category): ?>
-
-                  <a class="dropdown-item" href="<?php echo get_term_link($category); ?>"><?= $category->name; ?></a>
-
-                <?php endforeach; endif; ?>
-                  <div class="dropdown-divider"></div>
-                  <a class="dropdown-item text-success" href="<?= get_page_link(80);?>">Top 10</a>
-                  <a class="dropdown-item text-success" href="<?= get_page_link(72);?>">Random</a>
-                  <a class="dropdown-item text-success" href="<?= get_post_type_archive_link( 'quote' ); ?>">je veux tout !</a>
-                </div>
-              </li>
-            </ul>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
-            <a class="btn btn-primary btn-submit mr-5" href="<?= get_page_link(70); ?>" role="button"><span class="">Ta vision du monde ici !</span></a>
+
+          <!-- collapse navbar -->
+
+          <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+
+          <!-- dropdown categories -->
+
+            <div class="dropdown mr-5">
+                <button class="btn btn-secondary dropdown-toggle" type="button" id="categories" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  Catégories
+                </button>
+                <div class="dropdown-menu" aria-labelledby="categories">
+
+                  <?php $categories = $terms = get_Terms(array(
+                  'taxonomy' => 'categorie',
+                  'hide_empty' => false,
+                  ));
+
+                  if(is_array($categories)):
+
+                  foreach ($categories as $category): ?>
+
+                  <a href="<?php echo get_term_link($category); ?>">
+                    <button class="dropdown-item"><?= $category->name; ?>
+                    </button>
+                  </a> 
+
+                    <?php endforeach; endif; ?>
+
+                  <div class="dropdown-divider"></div>
+                    <a href="<?= get_page_link(80);?>">
+                      <button class="dropdown-item text-success">Top 10
+                      </button>
+                    </a>
+                    <a href="<?= get_page_link(72);?>">
+                      <button class="dropdown-item text-success">Random
+                      </button>
+                    </a>
+                    <a href="<?= get_post_type_archive_link( 'quote' ); ?>">
+                      <button class="dropdown-item text-success">Tout voir
+                      </button>
+                    </a>
+                  </div>
+              
+            </div>
+
+            <!-- submit button -->
+
+            <a class="btn btn-primary btn-submit mr-5" href="<?= get_page_link(70); ?>" role="button"><span class="">Ta vision du monde ici !</span>
+            </a>
+
+          <!-- search field -->
+
+
             <form class="form-inline my-2 my-lg-0 mr-5" method="get">
               <input class="form-control mr-sm-2 d-none d-sm-block" value="<?php the_search_query(); ?>" type="search" placeholder="Rechercher" aria-label="Search" name="s">
             </form>
@@ -71,7 +99,7 @@
             </div>
             <?php else: ?>
 
-            <a href="<?= get_page_link(11); ?>"> <button type="button" class="btn btn-warning ">Connexion/inscription</button></a>
+            <a href="<?= get_page_link(11); ?>"> <button type="button" class="btn btn-warning mr-5">Connexion/inscription</button></a>
 
             <?php endif; ?>
           </div>
