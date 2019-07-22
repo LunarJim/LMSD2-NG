b6822bfd1d067e6f052c4f274ad478d68851ba34
LunarJim <jim.deghaye@gmail.com>
2019-07-22 11:50:45 +0200

    changement du curseur par chargement d'une feuille de style custom / à coder en récupérant une value sur un form de la page mon compte (choix du curseur) puis chargement de la feuille de style en fonction du choix

--- content/themes/lmsdd/header.php
+++ content/themes/lmsdd/header.php
@@ -5,6 +5,7 @@
   <title>Le Monde se divise en 2</title>
   <link href="https://fonts.googleapis.com/css?family=Pangolin|Permanent+Marker|Roboto+Condensed" rel="stylesheet">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" href="<?= get_theme_file_uri();?>/custom-user-css/cursor1.css">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 <?php wp_head(); ?>
 </head>
