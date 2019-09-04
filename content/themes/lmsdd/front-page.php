3985957f0fe7ed35afad03bfd9d115a664032ce0
LunarJim <jim.deghaye@gmail.com>
2019-09-04 14:46:52 +0200

    modifications mineures d'indentation + ajout du tpl quote à la front-page

--- content/themes/lmsdd/front-page.php
+++ content/themes/lmsdd/front-page.php
@@ -1,15 +1,13 @@
 <?php get_header();?>
 
 <div class="main container posts">
-  <div class="row mb-4 pb-3">
+    <div class="row mb-4 pb-3">
 
-  <?php
-
-  // query des cpt quotes
-
+        <?php
+        // query des cpt quotes
         $args = [
-          'post_type'  => 'quote',
-          'posts_per_page' => 6,
+        'post_type'  => 'quote',
+        'posts_per_page' => 6,
         ];
 
         $quotes = new WP_Query($args);
@@ -17,37 +15,10 @@
         if ($quotes->have_posts()) : while ($quotes->have_posts()): $quotes->the_post();
         ?>
 
-        <div class="col-lg-4 col-sm-6 posts__sticker">
-    <div class="posts__content pl-1 pr-1 pt-1 pb-1 rounded shadow rounded font-weight-bold text-uppercase">
-        <div class="quote"><?php the_content(); ?></div>
-        
-        
-            <div class=" post__category">
+        <?php get_template_part('template-parts/quotes/quote'); ?>
 
-                <?php // affichage des catégories 
-
-                $terms = wp_get_post_terms(get_the_ID(),'categorie');
-
-                if(is_array($terms)): 
-
-                foreach ($terms as $term):  ?>
-
-                <a href="<?=get_term_link($term); ?>"> <button type="button" class="btn btn-secondary mb-1 mr-1 pl-1 pr-1 pt-0 pb-0"><?= $term->name; ?></button>
-                </a>
-
-                <?php endforeach; endif; ?>
-            </div>
-          <div class="vot_mp2" data-vote_id="<?php the_ID();?>">
-          </div>
-          <div><?php  get_favorites_button(get_the_ID()); ?></div>
-          <div class="font-italic text-lowercase pt-2 author"> <?php the_author_posts_link();?>
-          </div>
-    </div> 
-</div>
-          
-      
-      <?php endwhile; wp_reset_postdata(); endif; ?>
-  </div>
+        <?php endwhile; wp_reset_postdata(); endif; ?>
+    </div>
 </div>
 
 
--- content/themes/lmsdd/header.php
+++ content/themes/lmsdd/header.php
@@ -8,132 +8,130 @@
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css">
 
-  
-  
-<?php wp_head(); ?>
+  <?php wp_head(); ?>
 
-<?php
+  <?php get_user_cursor_type(); ?>
 
-  get_user_cursor_type(); ?>
 </head>
 
 
 <body <?php get_user_background_color(); ?>>
   <header class="container-fluid p-0 fixed-top shadow">
-      <nav class="navbar navbar-expand-lg navbar-light">
-          <a class="navbar-brand logo" href="<?= get_home_url() ?>">
-            <h1 class="logo mb-0">
-              <span class="logo__name mb-0">LMSD</span><i class="fas fa-hand-spock"></i>
-            </h1>
-            <p class="font-italic logo__slogan">Le monde se divise en 2</p>
-          </a>
-          
-
-          <!-- toggle button -->
-
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+    <nav class="navbar navbar-expand-lg navbar-light">
+      <a class="navbar-brand logo" href="<?= get_home_url() ?>">
+        <h1 class="logo mb-0">
+          <span class="logo__name mb-0">LMSD</span><i class="fas fa-hand-spock"></i>
+        </h1>
+        <p class="font-italic logo__slogan">Le monde se divise en 2
+        </p>
+      </a>
+      <!-- toggle button -->
+      <button class="navbar-toggler" type="button" 
+        data-toggle="collapse" 
+        data-target="#navbarSupportedContent" 
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false" 
+        aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon">
+        </span>
+      </button>
+      <!-- collapse navbar -->
+      <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+        <!-- dropdown categories -->
+        <div class="dropdown mr-3">
+          <button class="btn btn btn-outline-dark 
+            dropdown-toggle font-weight-bold"
+            type="button" id="categories" data-toggle="dropdown" 
+            aria-haspopup="true" aria-expanded="false">
+            Catégories
           </button>
-
-          <!-- collapse navbar -->
-
-          <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
-
-          <!-- dropdown categories -->
-
-            <div class="dropdown mr-3">
-                <button class="btn btn btn-outline-dark dropdown-toggle font-weight-bold" type="button" id="categories" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  Catégories
+            <div class="dropdown-menu" aria-labelledby="categories">
+              <?php if(is_user_logged_in()):?>
+              <a href="<?= get_page_link(88);?>">
+                <button class="dropdown-item text-success font-weight-bold">
+                Favoris
                 </button>
-                <div class="dropdown-menu" aria-labelledby="categories">
-                  <?php if(is_user_logged_in()):?>
-                <a href="<?= get_page_link(88);?>">
-                      <button class="dropdown-item text-success font-weight-bold">Favoris
-                      </button>
-                    </a>
-                  <?php endif; ?>
-
-                  <a href="<?= get_page_link(80);?>">
-                      <button class="dropdown-item text-success font-weight-bold">Top 10
-                      </button>
-                    </a>
-                    <a href="<?= get_page_link(72);?>">
-                      <button class="dropdown-item text-success">Random
-                      </button>
-                    </a>
-                    <a href="<?= get_post_type_archive_link( 'quote' ); ?>">
-                      <button class="dropdown-item text-success">Tout voir
-                      </button>
-                    </a>
-
-                    <div class="dropdown-divider"></div>
-
+              </a>
+                <?php endif; ?>
+              <a href="<?= get_page_link(80);?>">
+                <button class="dropdown-item text-success font-weight-bold">
+                Top 10
+                </button>
+              </a>
+              <a href="<?= get_page_link(72);?>">
+                <button class="dropdown-item text-success">
+                Random
+                </button>
+              </a>
+              <a href="<?= get_post_type_archive_link( 'quote' ); ?>">
+                <button class="dropdown-item text-success">
+                Tout voir
+                </button>
+              </a>
+              <div class="dropdown-divider"></div>
                   <?php $categories = $terms = get_Terms(array(
-                  'taxonomy' => 'categorie',
-                  'hide_empty' => false,
-                  ));
-
-
-                  if(is_array($categories)):
-
-                  foreach ($categories as $category): ?>
-
-                  <a href="<?php echo get_term_link($category); ?>">
-                    <button class="dropdown-item"><?= $category->name; ?><?=" ($category->count)" ?>
-                    </button>
-                  </a> 
-
+                    'taxonomy' => 'categorie',
+                    'hide_empty' => false,
+                    ));
+                    if(is_array($categories)):
+                    foreach ($categories as $category): 
+                  ?>
+                <a href="<?php echo get_term_link($category); ?>">
+                  <button class="dropdown-item">
+                    <?= $category->name; ?><?=" ($category->count)" ?>
+                  </button>
+                </a> 
                     <?php endforeach; endif; ?>
-
-                  </div>
-              
+              </div>
             </div>
-
-            
-
             <!-- submit button -->
-
             <span>
-              <a class="btn btn btn-outline-dark btn-submit mr-5 font-weight-bold" href="<?= get_page_link(70); ?>" role="button"><span class="">Ta citation</span>
+              <a class="btn btn btn-outline-dark btn-submit mr-5 font-weight-bold" 
+                href="<?= get_page_link(70); ?>" role="button">
+                <span>
+                Ta citation
+                </span>
               </a>
             </span>
-
-          <!-- search field -->
-
-
+            <!-- search field -->
             <form class="form-inline my-2 my-lg-0 mr-5" method="get">
-              <input class="form-control mr-sm-2 d-none d-sm-block" value="<?php the_search_query(); ?>" type="search" placeholder="Rechercher" aria-label="Search" name="s">
+              <input class="form-control mr-sm-2 d-none d-sm-block" value=
+              "<?php the_search_query(); ?>"
+               type="search" placeholder="Rechercher" 
+               aria-label="Search" name="s">
             </form>
-
             <!-- bouton de connexion/inscription qui est modifié si user connecté -->
-
             <?php if(is_user_logged_in()):?>
             <?php $current_user = wp_get_current_user(); ?>
-            
             <div class="dropdown">
-              <button class="btn btn btn-outline-dark dropdown-toggle font-weight-bold" type="button" id="dropdownMenu2"   data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              <button class="btn btn btn-outline-dark dropdown-toggle font-weight-bold"
+                type="button" id="dropdownMenu2" 
+                data-toggle="dropdown" 
+                aria-haspopup="true" 
+                aria-expanded="false">
                 Hello <?= $current_user->nickname ?> !
               </button>
-                <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
-                  <a href="<?= get_page_link(81);?>">
-                    <button class="dropdown-item" type="button">
-                      Mon compte
-                    </button>
-                  </a>
-                  <a href="<?= wp_logout_url(get_home_url());?>"> 
-                    <button class="dropdown-item" type="button">
-                      Déconnexion
-                    </button>
-                  </a>
-                </div>
+              <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
+                <a href="<?= get_page_link(81);?>">
+                  <button class="dropdown-item" type="button">
+                    Mon compte
+                  </button>
+                </a>
+                <a href="<?= wp_logout_url(get_home_url());?>"> 
+                  <button class="dropdown-item" type="button">
+                    Déconnexion
+                  </button>
+                </a>
+              </div>
             </div>
             <?php else: ?>
-
-            <a href="<?= get_page_link(11); ?>"> <button type="button" class="btn btn-warning mr-5">Connexion/inscription</button></a>
-
+            <a href="<?= get_page_link(11); ?>">
+              <button type="button" class="btn btn-warning mr-5">
+                Connexion/inscription
+              </button>
+            </a>
             <?php endif; ?>
-          </div>
-        </nav>
-    
-    
+        </div>  
+      </div>
+    </nav>
   </header>
