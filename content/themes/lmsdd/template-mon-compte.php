eba5f2ee04a1266841144141d064e12f0b78d3e2
LunarJim <jim.deghaye@gmail.com>
2019-09-05 10:17:00 +0200

    modifs mineures du tpl mon compte

--- content/themes/lmsdd/template-mon-compte.php
+++ content/themes/lmsdd/template-mon-compte.php
@@ -8,66 +8,72 @@ Template Post Type: page
 
 
 <div class="container main">
-        <div class="col-sm-12  pb-3">
-            <form action="" method="post">
-                <p>Choisir ma couleur de fond :</p>
+    <div class="col-sm-12 pb-3">
+        <form action="" method="post">
+            <p>
+                Choisir ma couleur de fond :
+            </p>
                 <div class="custom-control custom-radio">
-                    <input type="radio" id="customRadio1" name="color" value="background-color:aqua;" class="custom-control-input">
-                    <label class="custom-control-label" for="customRadio1">Bleu magique</label>
+                    <input type="radio" id="color-blue" name="color" 
+                    value="background-color:aqua;" class="custom-control-input">
+                    <label class="custom-control-label" for="color-blue">
+                        Bleu magique
+                    </label>
                 </div>
                 <div class="custom-control custom-radio">
-                    <input type="radio" id="customRadio2" name="color" value="background-color:red;" class="custom-control-input">
-                    <label class="custom-control-label" for="customRadio2">Rouge pompier</label>
+                    <input type="radio" id="color-red" name="color" 
+                    value="background-color:red;" class="custom-control-input">
+                    <label class="custom-control-label" for="color-red">
+                        Rouge pompier
+                    </label>
                 </div>
                 <div class="custom-control custom-radio">
-                    <input type="radio" id="customRadio3" name="color" value="background-color:yellow;" class="custom-control-input">
-                    <label class="custom-control-label" for="customRadio3">Jaune violent</label>
+                    <input type="radio" id="color-yellow" name="color" 
+                    value="background-color:yellow;" class="custom-control-input">
+                    <label class="custom-control-label" for="color-yellow">
+                        Jaune violent
+                    </label>
                 </div>
                 <div class="custom-control custom-radio">
-                    <input type="radio" id="customRadio4" name="color" value="background-image:
-                    radial-gradient(circle at top right,#C7CDEA,#DFDFDF); " class="custom-control-input">
-                    <label class="custom-control-label" for="customRadio4">défaut : magnifique dégradé</label>
+                    <input type="radio" id="color-gradient" name="color" 
+                    value="background-image:radial-gradient(circle at top right,#C7CDEA,#DFDFDF); " class="custom-control-input">
+                    <label class="custom-control-label" for="color-gradient">
+                        Défaut : magnifique dégradé
+                    </label>
                 </div>
-                <button type="submit" name="color-submit" class="btn btn-primary mt-2">Enregistrer la couleur</button>
-            </form>
-        </div>
+                <button type="submit" name="color-submit" 
+                class="btn btn-primary mt-2">
+                Enregistrer la couleur
+            </button>
+        </form>
+    </div>
 
         <div class="col-sm-12 pb-3">
             <form action="" method="post">
-                <p>Choisir mon curseur :</p>
+                <p>
+                    Choisir mon curseur :
+                </p>
                 <div class="custom-control custom-radio">
-                    <input type="radio" id="customRadio5" name="cursor" value="cursor1.css" class="custom-control-input">
-                    <label class="custom-control-label" for="customRadio5">Doigt magique</label>
+                    <input type="radio" id="cursor-finger" name="cursor" value="cursor1.css" class="custom-control-input">
+                    <label class="custom-control-label" for="cursor-finger">Doigt magique</label>
                 </div>
                 <div class="custom-control custom-radio">
-                    <input type="radio" id="customRadio6" name="cursor" value="cursor2.css" class="custom-control-input">
-                    <label class="custom-control-label" for="customRadio6">Sniper</label>
+                    <input type="radio" id="cursor-sniper" name="cursor" value="cursor2.css" class="custom-control-input">
+                    <label class="custom-control-label" for="cursor-sniper">Sniper</label>
                 </div>
                 <div class="custom-control custom-radio">
-                    <input type="radio" id="customRadio7" name="cursor" value="cursor-classic.css" class="custom-control-input">
-                    <label class="custom-control-label" for="customRadio7">Défaut : curseur</label>
+                    <input type="radio" id="cursor-default" name="cursor" value="cursor-classic.css" class="custom-control-input">
+                    <label class="custom-control-label" for="ccursor-default">Défaut : curseur</label>
                 </div>
-                <button type="submit" name="color-submit" class="btn btn-primary mt-2">Enregistrer mon curseur</button>
+                <button type="submit" name="cursor-submit" class="btn btn-primary mt-2">Enregistrer mon curseur</button>
             </form>
         </div>
-
-        <div class="col-sm-12  pb-3">
-            <form action="">
-                <p>Recevoir des alertes ?</p>
-                <div class="form-check">
-                    <input class="form-check-input" type="checkbox" name="mc4wp-subscribe" id="exampleRadios1" value="1">
-                    <label class="form-check-label" for="">
-                        Oui
-                    </label>
-                </div>
-                <button type="submit" class="btn btn-primary mt-2">Valider mon choix</button>
-            </form> 
-        </div>
-        <div class="col-sm-12 bg-white pt-3 pb-3 text-center ">
+        <div class="col-sm-12 bg-white pt-3 pb-3 mt-2 text-center ">
             <a class="" href="<?=wp_lostpassword_url();?>">
                 mot de passe oublié ?
             </a>
         </div>
+    </div>
 </div>
 
 
